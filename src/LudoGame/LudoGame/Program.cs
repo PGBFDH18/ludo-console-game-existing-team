68415799bc39cdd0a0447383a74d57570789835b
Aron1100 <35454815+Aron1100@users.noreply.github.com>
2019-01-08 15:27:43 +0100

    This surely works
    Testing things out

---
+++ src/LudoGame/LudoGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LudoGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("THIS IS WORKING FINE TRUST ME");
+        }
+    }
+}
